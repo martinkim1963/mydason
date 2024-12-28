7d91173ebb991e29e271701a7adcce3807cdcd35
martinkim1963 <144684794+martinkim1963@users.noreply.github.com>
2024-12-28 15:12:41 +0900

    panda-css 셋팅

---
+++ apps/web-view/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { css } from '../../styled-system/css';
+ 
+export default function Home() {
+  return (
+    <div className={css({ fontSize: "2xl", fontWeight: 'bold' ,_hover:{
+      color:'red'
+    }})}>Hello 🐼!</div>
+  )
+}
